fa4e099b5a5d149ed8fd06273147fb3a38985ef5
Mohamed Arafaath <mohamed_arafaath@outlook.com>
2021-05-21 20:23:14 +0530

    Create static_variable_recursion.cpp

---
+++ static_variable_recursion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fun(int n)
+{
+    static int x = 0;
+    if (n > 0)
+    {
+        x++;
+        return fun(n - 1) + x;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    int b = fun(5);
+    cout << b;
+    return 0;
+}
